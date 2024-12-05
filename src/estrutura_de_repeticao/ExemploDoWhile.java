b03b14225d293d213bddd00319cef541f7bfa336
Nathan <ribeiro.nathan2002@gmail.com>
2024-12-05 08:39:16 -0300

    Exemplos de laços de repitição

---
+++ src/estrutura_de_repeticao/ExemploDoWhile.java
@@ -0,0 +1,24 @@
+package estrutura_de_repeticao;
+
+import java.util.Scanner;
+
+public class ExemploDoWhile {
+
+	public static void main(String[] args) {
+
+        int numero, resultado, contador = 0;
+        Scanner leia = new Scanner(System.in);
+
+        do {
+            System.out.println("\nDigite um número inteiro: ");
+            numero = leia.nextInt();
+
+            resultado = numero * 5;
+            contador ++;
+            System.out.println("\nO resultado da multiplicação é: " + resultado);
+
+        } while (contador < 5);
+
+	}
+
+}
