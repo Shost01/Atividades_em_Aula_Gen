1e50a91cfe45aa66f0a2b3e4e8911a53e75399e3
Nathan <ribeiro.nathan2002@gmail.com>
2024-12-06 09:05:49 -0300

    Exemplos de vetor

---
+++ src/vetor/ExemploVetor.java
@@ -0,0 +1,16 @@
+package vetor;
+
+public class ExemploVetor {
+
+	public static void main(String[] args) {
+
+        String dogs[] = {"Boxer","Pastor Alemão","Pinscher","Husky Siberiano","Corgi"};
+        // Tipo nome_vetor[] = { valores }
+        
+        for(int i = 0; i < 5; i++) {
+            System.out.println((i + 1) + "º elemento: " + dogs[i]);  
+        }
+
+	}
+
+}
---
+++ src/vetor/ExenoloVetorUsuario.java
@@ -0,0 +1,29 @@
+package vetor;
+
+import java.util.Scanner;
+
+public class ExenoloVetorUsuario {
+
+	public static void main(String[] args) {
+
+		// tipo nome_vetor[] = new tipo[quantidade_valores];
+		
+        int numeros[] = new int[3]; // 0 - 1 - 2 - 3 
+        
+        Scanner leia = new Scanner(System.in);
+        
+        for(int indice = 0; indice < 3; indice ++) {
+            System.out.println("Digite o " + (indice + 1)  +"º Número: ");
+            numeros[indice] = leia.nextInt();
+        }
+        
+        System.out.println("\nOs números digitados foram: \n");
+        
+        for(int indice = 0; indice < 3; indice++) {
+            System.out.println((indice + 1) + "º número: " + numeros[indice]);
+        } 
+		
+
+	}
+
+}
